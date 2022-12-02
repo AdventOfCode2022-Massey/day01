f0baa4c773fbf356dc4cc8f0396ded93b7d2e84d
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-12-01 22:57:28 -0800

    Solution codes for Day 1

--- soln.rs
+++ soln.rs
@@ -8,8 +8,39 @@
 use aoc::*;
 
 fn main() {
+    
+    let mut sums = vec![];
+    let mut sum = 0u64;
+    for line in input_lines() {
+        if line.is_empty() {
+            sums.push(sum);
+            sum = 0;
+        }
+        else {
+            sum += line.parse().unwrap_or_else(|e|{
+                eprintln!("{line}: {e}");
+                0
+            });
+        }
+    }
+    if sum > 0 {
+        sums.push(sum);
+    }
+
     match get_part() {
-        Part1 => todo!(),
-        Part2 => todo!(),
+        Part1 => {
+            if let Some(max_sum) = sums.into_iter().max() { 
+                println!("{max_sum}");
+            }
+            else {
+                eprintln!("empty input");
+            }
+        }
+        Part2 => {
+            sums.sort();
+            let n = sums.len();
+            let max_sum = sums[n-3..].into_iter().sum::<u64>();
+            println!("{max_sum}");
+        }
     }
 }
