f19d38ee86c2e39ba1be028407be541eb38fbf3a
Bart Massey <bart.massey@gmail.com>
2022-12-04 14:25:41 -0800

    took rustfmt suggestions

--- soln.rs
+++ soln.rs
@@ -8,16 +8,14 @@
 use aoc::*;
 
 fn main() {
-    
     let mut sums = vec![];
     let mut sum = 0u64;
     for line in input_lines() {
         if line.is_empty() {
             sums.push(sum);
             sum = 0;
-        }
-        else {
-            sum += line.parse().unwrap_or_else(|e|{
+        } else {
+            sum += line.parse().unwrap_or_else(|e| {
                 eprintln!("{line}: {e}");
                 0
             });
@@ -29,17 +27,16 @@ fn main() {
 
     match get_part() {
         Part1 => {
-            if let Some(max_sum) = sums.into_iter().max() { 
+            if let Some(max_sum) = sums.into_iter().max() {
                 println!("{max_sum}");
-            }
-            else {
+            } else {
                 eprintln!("empty input");
             }
         }
         Part2 => {
             sums.sort();
             let n = sums.len();
-            let max_sum = sums[n-3..].iter().sum::<u64>();
+            let max_sum = sums[n - 3..].iter().sum::<u64>();
             println!("{max_sum}");
         }
     }
